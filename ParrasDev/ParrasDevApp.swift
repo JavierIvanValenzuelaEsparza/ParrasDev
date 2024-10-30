41e6b05475c1b30bbeb22c016326d917173d14c9
JavierValenzuelaEsparza <javier.valenzuela@g-global.com>
2024-10-29 19:06:49 -0700

    Initial Commit

---
+++ ParrasDev/ParrasDevApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParrasDevApp.swift
+//  ParrasDev
+//
+//  Created by Javier Valenzuela on 29/10/24.
+//
+
+import SwiftUI
+
+@main
+struct ParrasDevApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
